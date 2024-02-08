e06554063b450b0f54c744f02965bdbd94c27ccd
Shyam Ratnal <shyamprasad.ise@gmail.com>
2024-02-08 19:21:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header/Header';
+import AllRoute from './common/AllRoute';
+
+function App() {
+  return (
+    <div className="">
+      <Header/>
+      <AllRoute/>
+    </div>
+  );
+}
+
+export default App;
